35384e040fd976eb7f203da6be20da999bfe84ec
Jeremiah Egwa <jerriee232@gmail.com>
2025-05-13 03:15:06 +0100

    completed week1 assignment programming exercise

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+         Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 101);
+
+        // Ask user for a guess
+        int guess = 0;
+
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (guess > magicNum )
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess == magicNum )
+            {
+                Console.WriteLine("You guessed it!"); 
+            }
+            else
+            {
+                Console.WriteLine("Higer"); 
+            }
+        }
+
     }
 }
